7f9d78777940f5674b71abcc6ac3df04e5763fa6
Muhammed Sinan <muhammedsinan7749@gmail.com>
2025-02-23 22:17:10 +0530

    udpated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import { lazy, Suspense } from "react";
 import Loading from "./components/Loading";
+import SignupPage from "./pages/SignupPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProfilePage from "./pages/ProfilePage";
 
-const SignUpPage = lazy(() => import("@/pages/SignupPage"));
-const RegisterPage = lazy(() => import("@/pages/RegisterPage"));
-const ProfilePage = lazy(() => import("@/pages/ProfilePage"));
+// const SignUpPage = lazy(() => import("@/pages/SignupPage"));
+// const RegisterPage = lazy(() => import("@/pages/RegisterPage"));
+// const ProfilePage = lazy(() => import("@/pages/ProfilePage"));
 const NotFoundPage = lazy(() => import("@/pages/NotFoundPage"));
 
 const App = () => {
@@ -15,7 +18,7 @@ const App = () => {
       <Suspense fallback={<Loading />}>
         <Routes>
 
-          <Route path="/" element={<SignUpPage />} />
+          <Route path="/" element={<SignupPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/profile" element={<ProfilePage />} />
 
--- client/src/pages/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen  p-6">
+    <div className="flex flex-col items-center justify-center min-h-screen p-6">
       <h1 className="text-8xl font-bold mb-6">404</h1>
       <p className="text-lg mb-8">
         Oops! The page   does not exist.
